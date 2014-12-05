4203d0ea86f2c0952d41e96d46dcafe75b755f38
Utkarsh Jaiswal <utkarshjaiswal91@gmail.com>
2014-12-04 16:51:31 -0800

    Bugfixes for chord recognition logic

--- src/chord-recognizer.py
+++ src/chord-recognizer.py
@@ -7,10 +7,11 @@ from operator import itemgetter
 
 key = [] # key of current tab
 allowedKey = ['a','b','c','d','e','f','g','A','B','C','D','E','F','G'] # list of allowed tunings for strings
-allNotes = [] # list of all fret numbers and their corresponding positions (from the left) on that string
-# allNotes consists of a list of all (fret no, position) pairs in the tab 
+allNotes = [] # list of all string numbers, fret numbers and their corresponding positions (from the left) on that string
+# allNotes consists of a list of all (string no, fret no, position) triplets in the tab 
 stringCount = 1
 
+# prepare a list of all notes and their positions in the input tab
 with open("../data/ASCIItab.txt") as infile:
 	for line in infile:
 		if (stringCount > 6): # reset it
@@ -29,13 +30,35 @@ with open("../data/ASCIItab.txt") as infile:
 				allNotes.append([stringCount, lineNotes[i], linePos[i]])
 			stringCount += 1
 
-#allNotes = sorted(allNotes, key=itemgetter(1))
-print allNotes
-
-# Clean up key (tuning info)
-for char in key:
-	if not char.isalpha(): # remove non alphabetical characters (possible errors)
-		key.remove(char)
-key = key[0:6] # ensure a length of 6 since only six strings exist
+allNotes = sorted(allNotes, key=itemgetter(2)) # sort by position of individual notes 
+print allNotes, len(allNotes)
 print key
 
+# Run the set of notes passed to it (for a single chord) against the database
+# to see if it matches any chords
+def chordRecognition(chordNotes):
+	pass
+
+# Check for presence of chords
+# On the sorted list of notes, this is achieved by checking successive tuples to see if notes from different strings
+# are equidistant from the left, ie are played at the same time
+chordNotes = [] # (string no, fret no) pairs for chords
+i = 0
+while (i < len(allNotes) - 1):
+	x = allNotes[i]
+	y = allNotes[i+1]
+	if (x[2] == y[2]): # both are equidistant from the left
+		chordNotes.append(x)
+		chordNotes.append(y)
+		i += 1
+		if (i < len(allNotes) - 1):
+			y = allNotes[i+1]
+		while (x[2] == y[2] and i < len(allNotes) - 1):
+			chordNotes.append(y)
+			i += 1
+			if (i < len(allNotes) - 1):
+				y = allNotes[i+1]
+	#chordRecognition(chordNotes) # notes for one chord have been extracted, recognize it
+	i += 1
+	print 'chordNotes ', chordNotes
+	chordNotes = []
